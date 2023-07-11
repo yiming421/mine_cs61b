b8dce8912a901edceffb8c0a1293702d976cada4
yiming421 <2200013081@stu.pku.edu.cn>
2023-07-11 10:27:51 +0800

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,11 @@
+public class HelloNumber {
+    public static void main(String[] args){
+        int x = 0;
+        int sum = 0;
+        while(x < 10){
+            sum += x;
+            System.out.println(sum);
+            x += 1;
+        }
+    }
+}
